4a23e11c7d1ef12e5de53793d9e3d7503156d9a9
Jake Gadaleta <jake.gads@gmail.com>
2019-08-15 07:38:34 -0400

    listed out all of the commands just have to actual write all of the code now

---
+++ __init__.py
@@ -0,0 +1,1 @@
+# Here goes the from <filename> import <function>
---
+++ commands.py
@@ -0,0 +1,29 @@
+'''
+Commands
+
+Linux:      cd <loc>– Changes the current working directory in the command line console.
+Windows:    cd <loc>
+
+Linux:      exit – Exits out of the current program, terminates the current command line terminal, or logs you out of a Unix network depending on the context.
+Windows:    exit
+
+Linux:      kill – Terminates the specified running process. The Linux version of Windows’ “End Process” in the task manager. # This is gonna be an oof
+Windows:    taskkill /F /PID pid_number, taskkill /IM "process name" /F, taskkill /F /PID <some number>
+
+Linux:      ls – List all of the contents of a specified directory. If no directory is specified, it will use the current directory.
+Windows:    dir
+
+Linux:      pwd – Displays the current working directory for the command line terminal. Good for when you’ve lost track of where you are in your system.
+Windows:    cd
+
+Linux:      reboot – Immediately stops all running processes, shuts down the system, then reboots.
+Windows:    shutdown -r
+
+Linux:      shutdown – Stops all running processes and shuts down the system. Parameters can be specified to issue a delayed shutdown or a shutdown at a particular time.
+Windows:    shotdown -s
+
+Linux:      sudo – Runs commands as root, which means no limitations due to permissions.
+Windows:    1 Input: runas /profile /user:AAA “C:\programs\BBB.exe” 
+            2 Input the password of administrator AAA
+            3 Press “Enter”
+'''
